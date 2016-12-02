0f0ae2f984c7aa65429121583f721fa657bec7bc
varadih <helga.varadi1@gmail.com>
2016-12-02 20:58:55 +0100

    1st_exception

--- 1.py
+++ 1.py
@@ -2,12 +2,15 @@
 # and returns a new list with all its element's values doubled.
 # It should raise an error if the parameter is not a list.
 
-test=[1,2,3]
+test=[1, 2, 3]
 
 def double(list):
-    list_element_doubled=[]
-    for i in range(len(list)):
-        list_element_doubled.append(list[i] * 2)
-    return list_element_doubled
+    try:
+        list_element_doubled=[]
+        for i in range(len(list)):
+            list_element_doubled.append(list[i] * 2)
+        return list_element_doubled
+    except ValueError:
+        return("Make sure it's a list")
 
 print(double(test))
