59cb87da4faf79ea7793960be72a3c71c49609fb
varadih <helga.varadi1@gmail.com>
2016-12-02 20:34:19 +0100

    first excercise

--- 1.py
+++ 1.py
@@ -1,3 +1,13 @@
 # Create a function that takes a list as a parameter,
 # and returns a new list with all its element's values doubled.
 # It should raise an error if the parameter is not a list.
+
+test=[1,2,3]
+
+def double(list):
+    list_element_doubled=[]
+    for i in range(len(list)):
+        list_element_doubled.append(list[i] * 2)
+    return list_element_doubled
+
+print(double(test))
